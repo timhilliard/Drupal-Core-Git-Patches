ee86919664a60ca1c98da79dff7f35a5aeafdcd9
Maria McDowell <mgm.webwork@gmail.com>
2012-02-24 15:26:41 -0800

    Clarified usage of rmv_dpl_version_nums in comments

--- rmv_dpl-version_nums.sh
+++ rmv_dpl-version_nums.sh
@@ -1,4 +1,7 @@
-#Script removes drupal packing script info from core module and theme info files. - @acquia-MGM
+#Script removes drupal packing script info from core module and theme info files. 
+# run on older version of drupal, BEFORE applying patches.  
+# This does not remove Acquia Drupal packing informatoin.
+# - @mariagwyn with the  help of greggles.
 sed -i '' 's/; Information added by drupal.org packaging script on [0-9]*-[0-9]*-[0-9]*//g' modules/*/*.info themes/*/*.info
 sed -i '' 's/version = "[0-9]*.[0-9]*"//g' modules/*/*.info themes/*/*.info
 sed -i '' 's/project = "drupal"//g' modules/*/*.info themes/*/*.info
