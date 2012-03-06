27d9f63c2657ce76c9ec4bdb352ad46c4ff2aada
Maria McDowell <mgm.webwork@gmail.com>
2012-03-06 09:08:12 -0800

    Added patches, including patches to temp version of PF6.25

--- rmv_dpl-version_nums.sh
+++ rmv_dpl-version_nums.sh
@@ -5,8 +5,9 @@
 # must: chmod +x ~/rmv_version_nums.shn (makes executable)
 #
 # - @mariagwyn with the  help of greggles.
-sed -i '' 's/; Information added by drupal.org packaging script on [0-9]*-[0-9]*-[0-9]*//g' modules/*/*.info themes/*/*.info
-sed -i '' 's/version = "[0-9]*.[0-9]*"//g' modules/*/*.info themes/*/*.info
-sed -i '' 's/project = "drupal"//g' modules/*/*.info themes/*/*.info
-sed -i '' 's/datestamp = "[0-9]*"//g' modules/*/*.info themes/*/*.info
-sed -i '' '/^$/d' modules/*/*.info themes/*/*.info
+sed -i '' 's/; Information added by drupal.org packaging script on [0-9]*-[0-9]*-[0-9]*//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info
+sed -i '' 's/version = "[0-9]*.[0-9]*"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info
+sed -i '' 's/project = "drupal"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info
+sed -i '' 's/datestamp = "[0-9]*"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info
+sed -i '' '/^$/d' modules/*/*.info themes/*/*.info themes/tests/*/*.info
+echo "Version numbers removed"
